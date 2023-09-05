2dd8d1ea2e81a44b2c5fe38945478a1104f23139
JamesBoccuzzi <jamesboccuzzi123@gmail.com>
2023-09-04 22:25:11 -0400

    Update basics4.py

--- basics/basics4.py
+++ basics/basics4.py
@@ -1,17 +1,18 @@
 import random
 random_num = random.randrange(1,10)
-print(random_num)
-number = int(input("pick a number between 1-10 "))
+#print(random_num)
 attemps = 0
 while attemps < 3:
     attemps = attemps + 1
+    number = int(input("pick a number between 1-10 "))
     if number == random_num:
         print("You have selected the correct number")
+        break
     elif number > random_num:
         print("The number you have selected is too large")
     else:
         print("The number you have selected is too small")
-    number = int(input("pick a new number between 1-10 "))
+    
 
     
     
