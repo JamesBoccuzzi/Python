c641abcbabe2b5726422a9b75ad004f8f4b53378
JamesBoccuzzi <jamesboccuzzi123@gmail.com>
2023-09-16 22:40:50 -0400

    Variable Types Demo

---
+++ basics/variable_types.py
@@ -0,0 +1,23 @@
+print(type(1))
+print(type("a"))
+print(type([1,2,3]))
+print(type(True))
+print(type(3.5))
+print(type((1,4)))
+print(type({1:"james",
+            2:"frank",}))
+
+a = True
+print(type(a))
+
+b = 5.12
+print(type(b))
+
+c = 2,6
+print(type(c))
+
+d = [1,2,3,4,5]
+print(type(d))
+
+e = "hello"
+print(type(e))
