ec6bc6bd3872c943d15533031f5ab85607c53781
JamesBoccuzzi <jamesboccuzzi123@gmail.com>
2023-09-18 21:40:13 -0400

    For Loop Types

---
+++ basics/for_loop_type.py
@@ -0,0 +1,16 @@
+name = 'james'
+numbers = [1,2,3,4,5]
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+for letter in name:
+    print(letter)
+    
+for i in range(len(name)):
+    print(name[i])
+    
+for i in range(len(numbers)):
+    print(numbers[i])
